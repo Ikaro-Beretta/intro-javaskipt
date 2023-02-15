55e737e528cdbd1c98d6327c58ba5337cf8741e5
Ikaro-Beretta <124743573+Ikaro-Beretta@users.noreply.github.com>
2023-02-14 21:20:01 -0300

    aula variaveis

---
+++ exemplo/001-comandos-basicos.js
@@ -0,0 +1,35 @@
+console.log("hello world!");
+//comentario de uma linha
+/*comentario mais de uma linha
+
+*/
+
+/*
+  pesquisar a diferença entre as instruçoes
+  var, let e const na linguagem java-script
+  */
+
+  let nome = 'ikaro';
+  let idade = 27;
+  let altura = 1.92;
+  let trabalha = true;
+  let cores_favorita = ['preto','azul','rosa','vermelho'];
+  let endereco = {
+    'logradouro' : 'rua',
+    'nome_log'   : 'tupis',
+    'n'          : '20',
+    'bairro'     : 'centro'
+  };
+  let covid;
+  let dengue = null;
+  const exfunc = () => {};
+
+  console.log('variavel nome = '+ typeof(nome));
+  console.log('variavel idade = '+ typeof(idade));
+  console.log('variavel altura = '+ typeof(altura));
+  console.log('variavel trabalha = '+ typeof(trabalha));
+  console.log('variavel cores_favorita = '+ typeof(cores_favorita));
+  console.log('variavel endereco = '+ typeof(endereco));
+  console.log('variavel covid= '+ typeof(covid));
+  console.log('variavel dengue = '+ typeof(dengue));
+  console.log('variavel exfunc = '+ typeof(exfunc));
